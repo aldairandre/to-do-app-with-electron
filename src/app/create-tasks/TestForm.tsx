b72a9d6ee5fae8143d6cd21e49fc9eae6e622d0c
aldairandre <aldairandre99@gmail.com>
2023-10-16 18:25:30 +0100

    :recycle: Refactor

--- src/app/create-tasks/TestForm.tsx
+++ src/app/create-tasks/TestForm.tsx
@@ -1,17 +1,28 @@
 "use client"
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FieldProps } from 'formik';
 import * as Yup from "yup";
+
 
 const SignupSchema = Yup.object().shape({
   name: Yup.string()
     .min(2, 'Too Short!')
     .max(70, 'Too Long!')
     .required('Required'),
-  email: Yup.string()
-    .email('Invalid email')
-    .required('Required'),
+  description: Yup.string().required('Required')
 });
+
+const TextArea: React.FC<FieldProps> = ({ field, form, ...props }) => {
+  return (
+    <div className="py-2">
+      <label className="block text-gray-700 text-sm font-bold">Comentário:</label>
+      <textarea
+        {...field} {...props}
+        className="w-full h-24 p-2 mt-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 text-black"
+      />
+    </div>
+  )
+}
 
 
 function TestForm() {
@@ -20,37 +31,26 @@ function TestForm() {
       <Formik
         initialValues={{
           name: '',
-          email: '',
+          description: '',
         }}
         validationSchema={SignupSchema}
-        onSubmit={values => {
-          // same shape as initial values
+        onSubmit={(values, actions) => {
+          /* alert(JSON.stringify(values, null, 2)); */
           console.log(values);
+          actions.setSubmitting(false);
+          actions.resetForm()
         }}
       >
         {({ errors, touched }) => (
           <Form>
-            <Field name="name" className="mb-4" />
+            <Field name="name" className="mb-4 text-black" />
 
             {errors.name && touched.name ? (
               <div className='text-red-600'>{errors.name}</div>
             ) : null}
 
 
-            <Field name="email" type="email" />
-
-            {errors.email && touched.email ? (
-              <div className='text-red-600'>{errors.email}</div>
-            ) : null}
-
-            <div className="py-2">
-              <label className="block text-gray-700 text-sm font-bold">Comentário:</label>
-              <textarea
-                className="w-full h-24 p-2 mt-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-                placeholder="Digite seu comentário aqui..."
-              />
-            </div>
-
+            <Field name="description" placeholder="description" component={TextArea}/>
 
             <button type="submit" className='mt-8 py-5 w-1/2 bg-green-500 rounded-xl'>Submit</button>
 
